5015ccd84030c51239b63815f2e9998d44fb7949
Pav <101482555+Pavlonoz@users.noreply.github.com>
2024-03-09 18:35:49 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -31,20 +31,19 @@ local RequiredDistance, Typing, Running, Animation, ServiceConnections = 2000, f
 --// Script Settings
 
 Environment.Settings = {
-	Enabled = true,
-	TeamCheck = false,
-	AliveCheck = true,
-	WallCheck = false, -- Laggy
-	Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
-	ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
-	ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
-	TriggerKey = "MouseButton2",
-	Toggle = false,
-	LockPart = "Head" -- Body part to lock on
-    --// Variables for prediction
-    dropSetting = 0 -- Default value for drop setting
-    velocitySetting = 0 -- Default value for velocity setting
-    UsePrediction = false
+    Enabled = true,
+    TeamCheck = false,
+    AliveCheck = true,
+    WallCheck = false, -- Laggy
+    Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+    ThirdPerson = false, -- Uses mousemoverel instead of CFrame to support locking in third person (could be choppy)
+    ThirdPersonSensitivity = 3, -- Boundary: 0.1 - 5
+    TriggerKey = "MouseButton2",
+    Toggle = false,
+    LockPart = "Head", -- Body part to lock on
+    dropSetting = 0, -- Default value for drop setting
+    velocitySetting = 0, -- Default value for velocity setting
+    UsePrediction = false -- Toggle for prediction
 }
 
 Environment.FOVSettings = {
